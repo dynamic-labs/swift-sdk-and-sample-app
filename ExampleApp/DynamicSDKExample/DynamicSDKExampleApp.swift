f2b8d91d65972f355752433377de78d4d28b37fe
Nikola Garabandic <kantagara@gmail.com>
2026-02-12 14:51:54 +0100

    Fixing issue with EVM networks, creating one for b3 sepolia in the main
    [FIREHOG-BYPASS] These are not secrets

--- ExampleApp/DynamicSDKExample/DynamicSDKExampleApp.swift
+++ ExampleApp/DynamicSDKExample/DynamicSDKExampleApp.swift
@@ -4,17 +4,43 @@ import DynamicSDKSwift
 @main
 struct DynamicSDKExampleApp: App {
   init() {
-    // Initialize SDK at app launch (before SwiftUI renders views)
-    // Note: UIWindow overlay creation may be deferred if scene is not ready yet
+      
+      let b3SepoliaNetwork = GenericNetwork(
+          blockExplorerUrls: [
+              "https://sepolia-explorer.b3.fun"
+          ],
+          chainId: 1993,
+          chainName: "B3 Sepolia",
+          iconUrls: [
+              "https://icodrops.com/media/projects/covers/b3-fun_cover_1740116094.webp"
+          ],
+          lcdUrl: nil,
+          name: "B3 Sepolia",
+          nameService: nil,
+          nativeCurrency: NativeCurrency(
+              decimals: 18,
+              name: "Ether",
+              symbol: "ETH",
+          ),
+          networkId: 1993,
+          privateCustomerRpcUrls: nil,
+          rpcUrls: [
+              "https://sepolia.b3.fun"
+          ],
+          vanityName: nil
+      )
+      
+      
     _ = DynamicSDK.initialize(
       props: ClientProps(
-        environmentId: "a5c204fe-749e-466b-a584-8da2fb570ec1",
+        environmentId: "3e219b76-dcf1-40ab-aad6-652c4dfab4cc",
         appLogoUrl: "https://demo.dynamic.xyz/favicon-32x32.png",
         appName: "Dynamic Swift Demo",
         redirectUrl: "flutterdemo://",
         appOrigin: "https://demo.dynamic.xyz",
         logLevel: .debug,
-        debug: ClientDebugProps(webview: true)
+        debug: ClientDebugProps(webview: true),
+        evmNetworks: [b3SepoliaNetwork]
       )
     )
   }
